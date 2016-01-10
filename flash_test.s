dc5580532be5008c5d09bb49eae66ed155cca2e2
zhang <zz593141477@gmail.com>
2016-01-10 18:42:26 +0800

    erase flash in flash tests

--- flash_test.s
+++ flash_test.s
@@ -2,9 +2,12 @@
 .set noat
 .globl __start
 __start:
-  li $s0, 0x80000000
-  sb $0, 1($s0)
+  li $s1, 0xbfd00400
   li $s0, 0xbe000000
+
+  li $t0,0xffffffff #gpio0 all output
+  sw $t0,4($s1)
+
   li $t0, 0x90
   sw $t0, 0($s0)
   lw $t1, 0($s0) #Manufacture code
@@ -12,12 +15,19 @@ __start:
   sw $t0, 0($s0)
   lw $t1, 4($s0) #Device code
 
+  or $a0, $0, $s0
+  li $a1, 262144
+  sll $a2,$a1,6
+  addu $a2,$a2,$s0
+erase_next:
   li $t0, 0x20
   li $t1, 0xd0
-  sw $t0, 0($s0)
-  sw $t1, 0($s0)
-
+  sw $t0, 0($a0)
+  sw $t1, 0($a0)
+  sw $a0, 0($s1)
   jal wait_ready
+  addu $a0,$a0,$a1
+  bne $a0,$a2,erase_next
   nop
   li $t3, 0xbeef
 
