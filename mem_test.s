69d76ea1f01e5682e13f0da0a43b729ccc148c5a
ZhangYuxiang <zz593141477@gmail.com>
2017-01-20 14:02:52 +0800

    support for stride

--- mem_test.s
+++ mem_test.s
@@ -2,29 +2,33 @@
 .set noat
 .globl __start
 __start:
-  li $t7, 0xfaceface
-  li $t1, 0xa0000100
-  sw $t7, 0($t1)
-  lw $t7, 0($t1)
-  li $t0, 0x80000100
-  li $t1, 0x80002000
+  li $a0,0xbfd00400
+  li $a1,0xffffffff #gpio0 all output
+  sw $a1,4($a0)
+  sw $0,0($a0)
+  li $s0, 0x80000400 #start
+  li $s1, 0x80040000 #end
+  li $s2, 0x4 #stride
+re:
+  or $t0,$s0,$0
 wr_mem:
   sw $t0, 0($t0)
-  addiu $t0,$t0,4
-  bne $t0,$t1,wr_mem
+  addu $t0,$t0,$s2
+  bne $t0,$s1,wr_mem
   nop
-  li $t0, 0
+  b re
+  nop
+  or $t0,$s0,$0
 rd_mem:
   lw $t2, 0($t0)
   bne $t2,$t0,wrong
   nop
-  addiu $t0,$t0,4
-  bne $t0,$t1,rd_mem
+  addu $t0,$t0,$s2
+  bne $t0,$s1,rd_mem
   nop
-  b __start
-  nop
-
+  li $t0,0xffffffff
 wrong:
-  li $t7, 0xdeaddead
+  li $a0,0xbfd00400
+  sw $t0,0($a0)
   b wrong
   nop
