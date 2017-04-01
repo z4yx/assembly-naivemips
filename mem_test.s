a0e453bcab204d92341795a2f2bbc9ca8f4c192e
ZhangYuxiang <zz593141477@gmail.com>
2017-04-02 01:44:29 +0800

    test memory with random pattern

--- mem_test.s
+++ mem_test.s
@@ -6,23 +6,30 @@ __start:
   li $a1,0xffffffff #gpio0 all output
   sw $a1,4($a0)
   sw $0,0($a0)
-  li $s0, 0x80000400 #start
-  li $s1, 0x80040000 #end
+  li $s0, 0x80000200 #start, skip space of program itself
+  li $s1, 0x80800000 #end
   li $s2, 0x4 #stride
 re:
   or $t0,$s0,$0
+  li $t3,0xdeadbeef
+  li $t4,1103515245
 wr_mem:
-  sw $t0, 0($t0)
+  sw $t3, 0($t0)
+  mul $t3,$t3,$t4
+  addiu $t3,$t3,12345
   addu $t0,$t0,$s2
   bne $t0,$s1,wr_mem
   nop
-  b re
-  nop
+
   or $t0,$s0,$0
+  li $t3,0xdeadbeef
+  li $t4,1103515245
 rd_mem:
   lw $t2, 0($t0)
-  bne $t2,$t0,wrong
+  bne $t2,$t3,wrong
   nop
+  mul $t3,$t3,$t4
+  addiu $t3,$t3,12345
   addu $t0,$t0,$s2
   bne $t0,$s1,rd_mem
   nop
